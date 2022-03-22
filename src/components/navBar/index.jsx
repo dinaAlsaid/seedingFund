3abc4a9ea72823c8305786c718a681e8ccf78fd1
dinaAlsaid <41339383+dinaAlsaid@users.noreply.github.com>
2022-03-22 19:21:30 +0300

    setup router and nav bar

---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import mainLogo from "../../img/logo.png";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const NavBar = (props) => {
+  const navItems = [
+    { title: "home", link: "/", onClick: () => {} },
+    { title: "blog", link: "/portfolio", onClick: () => {} },
+    {
+      title: "portfolio",
+      link: "/portfolio",
+      onClick: () => {},
+    },
+    {
+      title: "pages",
+      link: "/pages",
+      onClick: () => {},
+    },
+    {
+      title: "features",
+      link: "/features",
+      onClick: () => {},
+    },
+    {
+      title: "Contact",
+      link: "/Contact",
+      onClick: () => {},
+    },
+    {
+      title: "Login",
+      link: "/",
+      onClick: () => {},
+    },
+  ];
+  return (
+    <Navbar bg="light" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={mainLogo} alt="logo" width="100" height="50" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {navItems.map((item) => (
+              <NavLink
+                to={item.link}
+                key={item.title}
+                className="nav-link"
+              >
+                {item.title}
+              </NavLink>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
